bd67da354d9b5e4ffa82a4b504967568e4f5f852
David Brands <david.brands@me.com>
2024-01-24 21:43:59 +0100

    WIP

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,24 @@
-const BASE_URL = 'http://localhost:3000'
-async function apiFetch(path: string) {
-    return await fetch(`${BASE_URL}${path}`)
+const BASE_URL = "http://localhost:3000";
+async function apiFetch(path: string, options?: RequestInit) {
+  return await fetch(`${BASE_URL}${path}`, {
+    ...options,
+    headers: {
+      ...options?.headers,
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 async function main() {
-    console.log(await apiFetch('/'))
+  console.log(
+    await apiFetch("/auth/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: "rohat@icloud.com",
+        password: "1234",
+      }),
+    }),
+  );
 }
 
-main()
+main();
