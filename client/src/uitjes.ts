81711f722dcd4d75685047dc1db08f80de3957d7
Sjors van Holst <sjorsvanholst@gmail.com>
2024-01-24 23:11:50 +0100

    ✨ Finished overview page

--- client/src/uitjes.ts
+++ client/src/uitjes.ts
@@ -13,12 +13,14 @@ async function loadUitjes() {
   for (const uitje of uitjes) {
     let total = 0;
 
-    uitje.users.forEach((user) => {
-      total += user.amount;
-    });
-
     const titleElement = document.createElement("h2");
     const totalElement = document.createElement("h3");
+    const usersElement = document.createElement("ul");
+
+    uitje.users.forEach((user) => {
+      total += user.amount;
+      usersElement.innerHTML += `<li>${user.name}  <span>/ € ${user.amount}</span></li>`;
+    });
 
     titleElement.textContent = uitje.title;
     totalElement.textContent = `€ ${total}`;
@@ -27,27 +29,8 @@ async function loadUitjes() {
 
     liElement.appendChild(titleElement);
     liElement.appendChild(totalElement);
+    liElement.appendChild(usersElement);
 
     ul.appendChild(liElement);
   }
 }
-
-// document
-//   .getElementById("signup-form")!
-//   .addEventListener("submit", async (event) => {
-//     event.preventDefault();
-
-//     const email = (document.getElementById("email") as HTMLInputElement).value;
-//     const name = (document.getElementById("name") as HTMLInputElement).value;
-//     const password = (document.getElementById("password") as HTMLInputElement)
-//       .value;
-
-//     const response = await apiFetch("/auth/signup", {
-//       method: "POST",
-//       body: JSON.stringify({
-//         name,
-//         email,
-//         password,
-//       }),
-//     });
-//   });
